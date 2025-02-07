67d782374f32d462816019c115e915f344a94728
Dinesh Pawar <dsp70458@gmail.com>
2025-02-07 18:58:38 +0530

    Enhance header responsiveness: Adjust logo size, reposition menu icon, and optimize mobile layout for better UX

--- frontend/src/components/Layout/Header.js
+++ frontend/src/components/Layout/Header.js
@@ -1,45 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { NavLink, Link } from "react-router-dom";
 import { FaSearch, FaHeart, FaShoppingCart } from 'react-icons/fa';
 import "../style/Header.css";
-import "../../pages/Signup";
-import "../../pages/Login";
-import "../../pages/HomePage";
-
-
 
 const Header = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [isSearchActive, setIsSearchActive] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(prevState => !prevState);
+  };
+
+  const toggleSearch = () => {
+    setIsSearchActive(prevState => !prevState);
+  };
+
   return (
     <>
       <header className="header">
         {/* Logo Section */}
         <div className='div-logo'>
-          <Link  className='logo' to="/">Rushi- The Cad Designer</Link>
+          <Link className='logo' to="/">Architecture Mart</Link>
+        </div>
+
+        {/* Hamburger Menu Icon */}
+        <div className="menu-icon" onClick={toggleMenu}>
+          <div></div>
+          <div></div>
+          <div></div>
         </div>
 
         {/* Navigation Menu */}
-        <nav className="nav-menu">
+        <nav className={`nav-menu ${isMenuOpen ? 'active' : ''}`}>
           <ul>
             <li>
-              <Link to="/">
-                Home
-              </Link>
+              <Link to="/">Home</Link>
             </li>
             <li>
-              <NavLink to="/category">
-                Category
-              </NavLink>
+              <NavLink to="/category">Category</NavLink>
             </li>
             <li>
-              <NavLink to="/pricing">
-                Pricing
-              </NavLink>
+              <NavLink to="/pricing">Pricing</NavLink>
             </li>
-          
             <li>
-              <NavLink to="/signup">
-                Register
-              </NavLink>
+              <NavLink to="/signup">Register</NavLink>
             </li>
             <li>
               <NavLink to="/login">
@@ -51,10 +55,13 @@ const Header = () => {
 
         {/* Header Icons */}
         <div className="header-icons">
-          <div className="search-bar">
-            <input type="text" placeholder="Search..." />
+          {/* Search Icon */}
+          <div className="search-bar" onClick={toggleSearch}>
             <FaSearch className="search-icon" />
+            <input type="text" placeholder="Search..." className={isSearchActive ? 'active' : ''} />
           </div>
+
+          {/* Cart and Heart Icons */}
           <FaHeart className="icon heart-icon" />
           <NavLink to="/cart">
             <FaShoppingCart className="icon cart-icon" />
@@ -67,76 +74,3 @@ const Header = () => {
 };
 
 export default Header;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// import React from "react";
-// import { NavLink, Link } from "react-router-dom";
-
-// const Header = () => {
-//   return (
-//     <>
-//       <nav className="navbar navbar-expand-lg bg-body-tertiary">
-//         <div className="container-fluid">
-//           <button
-//             className="navbar-toggler"
-//             type="button"
-//             data-bs-toggle="collapse"
-//             data-bs-target="#navbarTogglerDemo01"
-//             aria-controls="navbarTogglerDemo01"
-//             aria-expanded="false"
-//             aria-label="Toggle navigation"
-//           >
-//             <span className="navbar-toggler-icon" />
-//           </button>
-//           <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
-//             <Link to="/" className="navbar-brand">
-//               🛒 Ecommerce App
-//             </Link>
-//             <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
-//               <li className="nav-item">
-//                 <NavLink to="/" className="nav-link ">
-//                   Home
-//                 </NavLink>
-//               </li>
-//               <li className="nav-item">
-//                 <NavLink to="/category" className="nav-link ">
-//                   Category
-//                 </NavLink>
-//               </li>
-//               <li className="nav-item">
-//                 <NavLink to="/register" className="nav-link">
-//                   Register
-//                 </NavLink>
-//               </li>
-//               <li className="nav-item">
-//                 <NavLink to="/login" className="nav-link">
-//                   Login
-//                 </NavLink>
-//               </li>
-//               <li className="nav-item">
-//                 <NavLink to="/cart" className="nav-link">
-//                   Cart (0)
-//                 </NavLink>
-//               </li>
-//             </ul>
-//           </div>
-//         </div>
-//       </nav>
-      
-//     </>
-//   );
-// };
-
-// export default Header;
